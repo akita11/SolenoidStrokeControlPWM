41d3ee0019ee99d2d5ffeca6a542e1812ceab0b4
akita11 <akita@ifdl.jp>
2024-04-05 17:45:35 +0900

    adjust threshold

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,28 @@
 #include <M5Unified.h>
 
-#define PIN_PWM 22 // PortA-B (22/21/V/G)
-#define PIN_ADC 36 // PortB-B (36/26/V/G) // Vsol=12V / divider=4
-
+// PortB: 36/26/V/G
+#define PIN_PWM 26 // PortB
+#define PIN_ADC 36 // PortB / Vsol=12V / divider=4
 
 void setup() {
   M5.begin();
-  M5.Display.setTextSize(2);;
+  ledcSetup(0, 10, 8);
+  ledcAttachPin(PIN_PWM, 0);
+  ledcWrite(0, 20);
 }
 
-#define VTH1 1500 // [mV]
-#define VTH2 3200 // [mV]
+/*
+      +-----
+VTH2  |     | <----
+      |     -------+
+VTH1  |            |
+      |            |
+0 ----+            +-----
+      tm1   tm2
+*/
+
+#define VTH1 1500 // [mV], rising edge of PWM
+#define VTH2 2700 // [mV], dropping edge of PWM
 uint8_t st = 0;
 uint16_t tm1 = 0, tm2 = 0;
 
@@ -22,16 +34,11 @@ void loop() {
      tm1 = millis();
   }
   else if (st == 1 && v < VTH2){
-     st = 2;
-     tm2 = millis();
+      st = 2;
+      tm2 = millis();
+      printf(">t:%d\n", tm2 - tm1);
   }
   else if (st == 2 && v < VTH1){
      st = 0;
   }
-
-  M5.Display.clear();
-
-  M5.Display.setCursor(0, 0);
-  M5.Display.printf("%d", v);
-  M5.Display.printf("%d", tm2 - tm1);
 }
